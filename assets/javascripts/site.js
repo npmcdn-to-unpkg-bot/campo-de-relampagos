983987315ca543436d35f87519f5d0ea8aa7b20a
Alejandra Avilés <alehandra.aviles@gmail.com>
2016-03-29 20:37:26 +0200

    Add share blocks

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -23,4 +23,10 @@ $(document).ready(function() {
         window.open(url, 'fbShareWindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
         return false;
     });
+
+    $('.twitter-share').click(function(e) {
+    	e.preventDefault();
+    	window.open("https://twitter.com/share", 'twitterShareWindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+    	return false;
+    });
 });
