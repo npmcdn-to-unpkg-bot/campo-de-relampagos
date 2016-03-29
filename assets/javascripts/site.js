30d91ced06efb760b74db28118844bf39c4ed6cb
Alejandra Avilés <alehandra.aviles@gmail.com>
2016-03-29 20:20:12 +0200

    Add changes

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -9,7 +9,18 @@ $('.masonry-grid-guttered').masonry({
 
 $('#search-trigger').click(function(e) {
 	$('#search').toggle();
+	$('#search input').focus();
 	e.preventDefault();
 });
 
 $('.sticky').sticky({ topSpacing: 0 });
+
+$(document).ready(function() {
+    $('.fb-share').click(function(e) {
+        e.preventDefault();
+        var pageUrl = window.location.toString();
+        var url = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(pageUrl);
+        window.open(url, 'fbShareWindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+        return false;
+    });
+});
